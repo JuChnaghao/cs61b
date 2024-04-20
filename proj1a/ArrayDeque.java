0f1175079492a68a99a3d054c80e33a73374059e
JU Changhao <3347386445@qq.com>
2024-04-20 14:32:12 +0800

    changed ArrayDeque to version 3

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,4 +1,7 @@
-public class ArrayDeque<T> {
+import java.lang.foreign.SymbolLookup;
+
+public class ArrayDeque<T>
+{
     private int size;
     private int start,top;
     private T[] memory ;
@@ -7,56 +10,77 @@ public class ArrayDeque<T> {
     {
         length = 8;
         size = 0;
-        start = length - 1;
+        start = 1;
         top = 0;
         memory = (T[]) new Object[8];
     }
-    private void resize(int capacity)
+    private int plusone(int index, int length)
     {
-        if(start + 1 >= top)
+        if (index + 1 == length)
+            return 0;
+        return index + 1;
+    }
+    private int minusone(int index, int length)
+    {
+        if (index == 0)
+            return length - 1;
+        return index - 1;
+    }
+    private void grow()
+    {
+        int index1 = 0;
+        int index2 = start;
+        T[] newarray = (T[]) new Object[length * 2];
+        for (int i = 0; i < size; i++)
         {
-            T[] narray = (T[]) new Object[capacity];
-            System.arraycopy(memory,0,narray,0,top);
-            System.arraycopy(memory,start+1,narray,capacity-memory.length+start+1,memory.length-start-1);
-            memory = narray;
-            length = capacity;
-            start = length - 1 - (size-top);
+            newarray[index1] = memory[index2];
+            index1 = plusone(index1, 2 * length);
+            index2 = plusone(index2, length);
         }
-        else
+        start = 0;
+        top = minusone(index1, 2 * length);
+        length *= 2;
+        memory = newarray;
+    }
+    private void shrink()
+    {
+        int index1 = 0;
+        int index2 = start;
+        T[] newarray = (T[]) new Object[length / 2];
+        for (int i = 0; i < size; i++)
         {
-            T[] narray = (T[]) new Object[capacity];
-            System.arraycopy(memory,start+1,narray,start+1,size);
-            memory = narray;
-            length = capacity;
+            newarray[index1] = memory[index2];
+            index1 = plusone(index1, length / 2);
+            index2 = plusone(index2, length);
         }
+        start = 0;
+        top = minusone(index1, length / 2);
+        length /= 2;
+        memory = newarray;
     }
     public void addFirst(T item)
     {
         if(size == length)
         {
-            resize(2*size);
+            grow();
         }
-        memory[start]=item;
-        start -= 1;
-        start = start%length;
+        start = minusone(start, length);
+        memory[start] = item;
         size += 1;
     }
     public void addLast(T item)
     {
         if(size == length)
         {
-            resize(2*size);
+            shrink();
         }
+        top = plusone(top, length);
         memory[top] = item;
-        top += 1;
-        top = top%length;
         size += 1;
     }
     public boolean isEmpty()
     {
-        if(size == 0)
-            return true;
-        return false;
+        return size == 0;
     }
     public int size()
     {
@@ -64,37 +88,33 @@ public class ArrayDeque<T> {
     }
     public void printDeque()
     {
-        for(int i = start+1; i < length; i++)
-        {
-            System.out.print(memory[i] + " ");
-        }
-        for(int i = 0; i < top; i++)
+        for(int i = start; i != plusone(top, length); i = plusone(i, length))
         {
             System.out.print(memory[i] + " ");
         }
     }
     public T removeFirst()
     {
-        T result;
-        start += 1;
-        start %= length;
-        result = memory[start];
+        T result = memory[start];
         memory[start] = null;
+        start = plusone(start, length);
         size -= 1;
-        if(size*4 <= length)
-            resize(length/2);
+        if(size*4 < length)
+        {
+            shrink();
+        }
         return result;
     }
     public T removeLast()
     {
-        T result;
-        top -= 1;
-        top %= length;
-        result = memory[top];
+        T result = memory[top];
         memory[top] = null;
+        top = minusone(top, length);
         size -= 1;
-        if(size*4 <= length)
-            resize(length/2);
+        if(size*4 < length)
+        {
+            shrink();
+        }
         return result;
     }
     public T get(int index)
@@ -103,7 +123,7 @@ public class ArrayDeque<T> {
             return null;
         else
         {
-           return memory[(start+1+index)%length];
+           return memory[(start+index)%length];
         }
     }
 }
