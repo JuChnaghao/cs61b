10a4f4bd84fde923fb4752b2b41de9650158a4f6
JU Changhao <3347386445@qq.com>
2024-04-20 21:32:41 +0800

    corected some errors

--- proj1b/Palindrome.java
+++ proj1b/Palindrome.java
@@ -1,7 +1,7 @@
 public class Palindrome {
     public Deque<Character> wordToDeque(String word)
     {
-        Deque<Character> result = new LinkedListDeque<>();
+        Deque<Character> result = new LinkedListDeque();
         for(int i=0;i<word.length();i++)
         {
             result.addLast(word.charAt(i));
