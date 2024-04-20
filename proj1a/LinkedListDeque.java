4fc1c64e1b03be9ec2313833d6b32f6b229e80d3
JU Changhao <3347386445@qq.com>
2024-04-20 11:23:59 +0800

    changed LinkedListDeque to version 2

--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -1,97 +1,100 @@
-import java.util.LinkedList;
-
 public class LinkedListDeque<T> {
-    public T item;
-    public LinkedListDeque<T> next,front;
-    public int size;
+    private static class Node<T>{
+        public T item;
+        public Node next;
+        public Node last;
+        public Node(T i, Node n, Node l){
+            item = i;
+            next = n;
+            last = l;
+        }
+    }
+    private Node sentinel;
+    private int size;
     public LinkedListDeque()
     {
-        item = null;
-        next = front = this;
-        size = 0;
+        sentinel = new Node(null,null,null);
+        sentinel.next = sentinel;
+        sentinel.last = sentinel;
+        size=0;
     }
     public void addFirst(T item)
     {
-        LinkedListDeque<T> added = new LinkedListDeque<T>();
-        LinkedListDeque<T> temp = new LinkedListDeque<T>();
-        added.item = item;
-        temp = this.next;
-        this.next=added;
-        added.front=this;
-        temp.front=added;
-        added.next=temp;
         size+=1;
+        Node temp = new Node(item,null,null);
+        temp.next = sentinel.next;
+        sentinel.next.last = temp;
+        sentinel.next = temp;
+        temp.last = sentinel;
     }
     public void addLast(T item)
     {
-        LinkedListDeque<T> added = new LinkedListDeque<T>();
-        LinkedListDeque<T> temp = new LinkedListDeque<T>();
-        added.item = item;
-        temp = this.front;
-        this.front=added;
-        added.next=this;
-        temp.next=added;
-        added.front=temp;
         size+=1;
+        Node temp = new Node(item,null,null);
+        temp.last = sentinel.last;
+        sentinel.last.next = temp;
+        sentinel.last = temp;
+        temp.next = sentinel;
     }
     public boolean isEmpty()
     {
-        if(this.size==0)
+        if(size == 0)
+        {
             return true;
+        }
         return false;
     }
     public int size()
     {
-        return this.size;
+        return size;
     }
     public void printDeque()
     {
-        LinkedListDeque<T> flag=this.next;
-        while(flag.item!=null)
+        Node p = sentinel.next;
+        while(p != sentinel)
         {
-            System.out.print(flag.item + " ");
-            flag=flag.next;
+            System.out.print(p.item + " ");
+            p = p.next;
         }
     }
     public T removeFirst()
     {
-        if(this.size==0)
+        if(size == 0)
+        {
             return null;
+        }
         else
         {
-            T result=this.next.item;
-            this.next.next.front=this;
-            this.next=this.next.next;
             size-=1;
-            return result;
+            T temp = (T) sentinel.next.item;
+            sentinel.next=sentinel.next.next;
+            return temp;
         }
     }
     public T removeLast()
     {
-        if(this.size==0)
+        if(size == 0)
+        {
             return null;
+        }
         else
         {
-            T result=this.front.item;
-            this.front.front.next=this;
-            this.front=this.front.front;
             size-=1;
-            return result;
+            T temp = (T) sentinel.last.item;
+            sentinel.last=sentinel.last.last;
+            return temp;
         }
     }
     public T get(int index)
     {
-        if (index>=this.size)
+        Node p = sentinel;
+        while(index >= 0)
         {
-            return null;
+            p = p.next;
+            index -= 1;
         }
-        int result=0;
-        LinkedListDeque<T> flag=this.next;
-        while(result<index)
-        {
-            result+=1;
-            flag=flag.next;
-        }
-        return flag.item;
+        return (T) p.item;
     }
+
+
 }
