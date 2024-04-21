eebe79033214bbfb3bc4820e4117675db85fcc9d
JU Changhao <3347386445@qq.com>
2024-04-21 14:54:30 +0800

    updated the OffByOne

--- proj1b/OffByOne.java
+++ proj1b/OffByOne.java
@@ -2,8 +2,6 @@ public class OffByOne implements CharacterComparator{
     @Override
     public boolean equalChars(char x, char y)
     {
-        if(x==y+1 || x+1==y)
-            return true;
-        return false;
+       return Math.abs(x - y) == 1;
     }
 }
