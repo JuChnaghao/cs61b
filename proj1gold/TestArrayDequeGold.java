7a404b796f13da20a403d1ad4f719c6dbdf7360c
JU Changhao <3347386445@qq.com>
2024-04-22 11:58:51 +0800

    complicate the project 1 Gold

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -6,33 +6,39 @@ public class TestArrayDequeGold {
         ArrayDequeSolution<Integer> l1 = new ArrayDequeSolution<>();
         StudentArrayDeque<Integer> l2 = new StudentArrayDeque<>();
         int num = 100;
-        int size=0;
+        int size = 0;
         String operations = "";
         while (true) {
-            double numberBetweenZeroAndFour = 4*StdRandom.uniform();
-            if (numberBetweenZeroAndFour < 1) {
+            int numberBetweenZeroAndFour = StdRandom.uniform(4);
+            if (numberBetweenZeroAndFour == 0) {
                 Integer x = StdRandom.uniform(num);
                 l1.addFirst(x);
                 l2.addFirst(x);
                 size++;
                 operations += String.format("addFirst(%d)\n",x);
-            } else if (numberBetweenZeroAndFour < 2) {
+            } else if (numberBetweenZeroAndFour == 1) {
                 Integer x = StdRandom.uniform(num);
                 l1.addLast(x);
                 l2.addLast(x);
                 size++;
                 operations += String.format("addLast(%d)\n",x);
-            } else if (numberBetweenZeroAndFour < 3) {
+            } else if (numberBetweenZeroAndFour == 2) {
                 if(size != 0){
-                    operations += "removefirst()\n";
-                    assertEquals(operations, l1.removeFirst(), l2.removeFirst());
+                    Integer remove1,remove2;
+                    remove1 = l1.removeFirst();
+                    remove2 = l2.removeFirst();
+                    operations += "removeFirst()\n";
+                    assertEquals(operations, remove1, remove2);
                     size--;
                 }
             }
             else {
                 if(size != 0) {
+                    Integer remove1,remove2;
+                    remove1 = l1.removeLast();
+                    remove2 = l2.removeLast();
                     operations += "removeLast()\n";
-                    assertEquals(operations, l1.removeLast(), l2.removeLast());
+                    assertEquals(operations, remove1, remove2);
                     size--;
                 }
             }
